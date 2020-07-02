f30abfd34cc753f5ae6545fdb17cd3f930b5153e
Abhishek Chaudhary <chaudha4@hotmail.com>
2020-07-02 13:01:33 -0400

    Add Play Button

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ function App() {
   const [quote, setQuote] = useState([]);
   const [db, setDb] = useState(0);
 
-  // Similar to instance variable in a class.
+  // Similar to instance variable in a class. Essentially, useRef 
+  // is like a “box” that can hold a mutable value in its .current property.
+
   const indx = useRef(1);
+  const myTimer = useRef(null);
 
   /**
    * Similar to componentDidMount and componentDidUpdate.
@@ -38,6 +41,24 @@ function App() {
       });
   }, []);
 
+  const getNewQuote = () => {
+    setQuote(db[indx.current]); // Access the current value and then increment it.
+
+    indx.current = (indx.current < db.length - 1) ? indx.current + 1 : 0;
+
+    console.log("Index is now %s and max is %s", indx.current, db.length);
+  }
+
+  const playQuote = () => {
+    myTimer.current = setInterval(() => {
+      getNewQuote();
+    }, 1000)
+  }
+
+  const stopQuote = () => {
+    clearInterval(myTimer.current);
+  }  
+
   return (
     <div className="container">
       <h1 className="title">Random Quote Generator</h1>
@@ -46,15 +67,24 @@ function App() {
         "{quote.text}"
       </div>
 
-      <div className="author">{quote.author}</div>
+      <div className="author">
+        {quote.author ? quote.author : "Anonymous"}
+      </div>
+
       <div className="center">
-        <button className="button" onClick={() => {
-          setQuote(db[indx.current]);
-          indx.current = indx.current + 1;
-          console.log("Index is now " + indx.current);
-        }}>
+        <button className="button margin" onClick={getNewQuote}>
           Get a new Quote
-      </button>
+        </button>
+      </div>
+
+      <div className="center">
+        <button className="button" onClick={playQuote}>
+          Play
+        </button>
+        <button className="button" onClick={stopQuote}>
+          Stop
+        </button>
+
       </div>
 
     </div>
